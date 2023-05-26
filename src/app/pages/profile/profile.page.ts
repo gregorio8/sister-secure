b4738f4c745d11c9385f02f29a10d7ee9656639f
Leonardo Lima <leonardo.lima.gomes2003@gmail.com>
2023-05-25 21:13:29 -0300

    I implemented business logic to change the password.

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RegisterService } from '../register/register.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -10,9 +12,11 @@ export class ProfilePage implements OnInit {
 
   userData: any = {};
   showPassword: boolean = false;
+  newPassword : string = "";
 
-  constructor(private registerService: RegisterService) {
+  constructor(private registerService: RegisterService, private alertController: AlertController, private router: Router) {
     this.userData = this.registerService.userData;
+    
   }
 
   ngOnInit() {
@@ -24,6 +28,45 @@ export class ProfilePage implements OnInit {
     event.preventDefault();
     event.stopPropagation(); 
     this.showPassword = !this.showPassword;
+  }
+
+  async changePassword(newPassword: string) {
+    this.userData.password = newPassword;
+    await this.registerService.saveNewUserData(newPassword);
+    console.log('Senha alterada com sucesso!');
+  }
+
+
+  async openChangePasswordDialog() {
+    const alert = await this.alertController.create({
+      header: 'Altere sua Senha',
+      inputs: [
+        {
+          name: 'newPassword',
+          type: 'password',
+          placeholder: 'Nova Senha'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Voltar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Ação cancelada');
+          }
+        },
+        {
+          text: 'Alterar',
+          cssClass: 'xuxa',
+          handler: (data) => {
+            this.changePassword(data.newPassword)
+            console.log('Ação executada');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
